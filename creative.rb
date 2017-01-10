5de0fbf61de20392145cd4f82f37d3ff1b2e7daf
Alastair Hood <alastair.hood85@gmail.com>
2017-01-10 17:03:37 +0000

    FINAL

--- creative.rb
+++ creative.rb
@@ -1,4 +1,23 @@
-# here
+# a function to offer a tentative identification of bat species based on frequency of recordings in kHz
+
+
+def identify(freq1, freq2, freq3)
+  averagefreq = (freq1 + freq2 + freq3) / 3
+
+if averagefreq > 70
+  return "Identification not possible."
+elsif averagefreq <= 70 && averagefreq > 50
+  return "Soprano pipistrelle"
+elsif averagefreq == 50
+  return "Species in pipistrelle genus"
+elsif averagefreq <= 49 && averagefreq > 30
+  return "Long-eared bat"
+else
+  return "Identification not possible."
+end
+end
+
+  puts identify(34, 53, 67)
 
 
 
